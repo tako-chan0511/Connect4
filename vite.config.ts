93970ba233880d189630d28a9814e163c9bfbb6a
Hara Keisuke <harakeisuke7@gmail.com>
2025-05-23 16:44:51 +0900

    vercel-5

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// Vercel 上では process.env.VERCEL が "1" になる想定
-const isVercel = !!process.env.VERCEL
-
-export default defineConfig(({ command }) => ({
-  base:
-    // 開発サーバー（npm run dev）とプレビュー（npm run preview）のときはルート配下
-    command === 'serve'
-      ? '/'
-      // ビルド（npm run build）して GitHub Pages に deploy するときのみ `/Connect4/`
-      : isVercel
-        ? '/'        // Vercel 本番はルート
-        : '/Connect4/',
+// GitHub Pages では /Connect4/、それ以外（Vercel やローカルプレビュー）ではルート参照
+export default defineConfig({
+  base: process.env.VERCEL ? '/' : '/Connect4/',
   plugins: [vue()],
-}))
+})
