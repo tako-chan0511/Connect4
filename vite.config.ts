94c4a5cd5e8ef621ef5f1ddc607f861d0b1747d8
Hara Keisuke <harakeisuke7@gmail.com>
2025-05-23 16:03:03 +0900

    Vercel対応２

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(() => {
-  // Vercel 上では import.meta.env.VERCEL が "1" など truthy
-  const isVercel = Boolean(import.meta.env.VERCEL)
+export default defineConfig(({ command, mode }) => {
+  // env ファイルも含めて読み込む
+  const env = loadEnv(mode, process.cwd(), '')
+
+  // Vercel のビルド環境では VERCEL=1 が渡されます
+  const isVercel = env.VERCEL === '1'
+
+  // 開発サーバー時は常にルート '/', ビルド時だけ GitHub Pages 用に '/Connect4/'
+  const base =
+    command === 'serve'
+      ? '/'
+      : isVercel
+        ? '/'
+        : '/Connect4/'
+
   return {
-    base: isVercel ? '/' : '/Connect4/',
+    base,
     plugins: [vue()],
   }
 })
